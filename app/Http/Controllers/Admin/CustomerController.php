c9801c5547b2591337bdabbb936160bbac719e81
Amr Mahmoud <amrmgoma6@gmail.com>
2023-12-24 00:37:58 +0200

    changed  fields handeled by admin

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -42,41 +42,14 @@ class CustomerController extends Controller
         }
 
         $request->validate([
-            'name' => 'required|string',
-            'phone' => 'required|integer|digits_between:6,11',
-            'email' => 'required|email',
-            'password' => 'nullable|min:6',
             'wallet_balance' => 'required|numeric'
         ]);
 
-
-        if ($resp = $this->handlePhoneErrors($request, $customerId)) {
-            return $resp;
-        }
-
-        if ($resp = $this->handleEmailErrors($request, $customerId)) {
-            return $resp;
-        }
-
-
-
-        if ($request->password) {
-            $password = Hash::make($request->password);
-        } else {
-            $password = $customer->password;
-        }
-
-        DB::update("UPDATE customers SET name = :name, phone = :phone,
-         email = :email, password = :password,
-         wallet_balance = :wallet_balance
+        DB::update("UPDATE customers SET wallet_balance = :wallet_balance
         WHERE id = :id
         ", [
             ":id" => $customerId,
-            ":name" => $request->name,
-            ":phone" => $request->phone,
-            ":email" => $request->email,
-            ":password" => $password,
-            ":wallet_balance" => $request->wallet_balance,
+            ":wallet_balance" => $request->wallet_balance
         ]);
 
         return response()->json([
@@ -93,40 +66,5 @@ class CustomerController extends Controller
         return response()->json([
             'success' => true,
         ]);
-    }
-
-
-    private function handlePhoneErrors($request, $customerId = 0)
-    {
-        // check if phone is not used before
-        // if I am updating ignore th current customer$customerId
-        $bindings = [$request->phone];
-        $ignoreCustomer = "";
-        if ($customerId) {
-            $ignoreCustomer = " AND id != ?";
-            $bindings[] = $customerId;
-        }
-        $result = DB::selectOne("select * from customers where phone = ? " . $ignoreCustomer, $bindings);
-        if ($result) {
-            return $this->errorResponse(["phone" => ["Phone is used before are you (" . $result->name . ")"]], 422); // validation error
-        }
-        return false;
-    }
-
-    private function handleEmailErrors($request, $customerId = 0)
-    {
-        // check if email is not used before
-        // if I am updating ignore th current customer$customerId
-        $bindings = [$request->email];
-        $ignoreCustomer = "";
-        if ($customerId) {
-            $ignoreCustomer = " AND id != ?";
-            $bindings[] = $customerId;
-        }
-        $result = DB::selectOne("select * from customers where email = ? " . $ignoreCustomer, $bindings);
-        if ($result) {
-            return $this->errorResponse(["email" => ["Email is used before are you (" . $result->name . ")"]], 422); // validation error
-        }
-        return false;
     }
 }
